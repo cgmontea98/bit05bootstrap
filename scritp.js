f5e8ac8c1ccde7f216a5c8c013eb780d760e177c
cgmontea98 <cgmontealegre9804@gmail.com>
2022-12-11 20:18:43 -0500

    rediseño en catalogo y footer

--- scritp.js
+++ scritp.js
@@ -1,17 +1,17 @@
 //checkbox de Marca
 let check1 = document.getElementById("coca");
-/* let check2 = document.getElementById("sprite");
+ let check2 = document.getElementById("sprite");
 let check3 = document.getElementById("quatro");
 let check4 = document.getElementById("del_Valle");
 let check5 = document.getElementById("powerade");
-let check6 = document.getElementById("snacks"); */
+let check6 = document.getElementById("snacks");
 //eventos de los checks
 check1.addEventListener("change", filtro1, false);
-/* check2.addEventListener('change', filtro2, false)
-check3.addEventListener('change', filtro3, false)
-check4.addEventListener('change', filtro4, false)
-check5.addEventListener('change', filtro5, false)
-check6.addEventListener('change', filtro6, false) */
+check2.addEventListener('change', filtro1, false)
+check3.addEventListener('change', filtro1, false)
+check4.addEventListener('change', filtro1, false)
+check5.addEventListener('change', filtro1, false)
+check6.addEventListener('change', filtro1, false) 
 
 function filtro1() {
   var checked = check1.checked;
@@ -20,11 +20,4 @@ function filtro1() {
   }
 }
 
-
-let cads = document.getElementById('productos')
-let prb = document.getElementById('prueba')
-
-let arreglocads = [].slice.call(cads);
-prb = arreglocads instanceof Array
-console.log(prb)
 
