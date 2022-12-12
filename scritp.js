f27b8a6526beb5e8d82f339d90777b388b37975e
cgmontea98 <cgmontealegre9804@gmail.com>
2022-12-12 13:07:08 -0500

    js

--- scritp.js
+++ scritp.js
@@ -1,24 +1,36 @@
 /* //checkbox de Marca
-let check1 = document.getElementById("coca");
- let check2 = document.getElementById("sprite");
+
+let check2 = document.getElementById("sprite");
 let check3 = document.getElementById("quatro");
 let check4 = document.getElementById("del_Valle");
 let check5 = document.getElementById("powerade");
 let check6 = document.getElementById("snacks");
 //eventos de los checks
-check1.addEventListener("change", filtro1, false);
+
 check2.addEventListener('change', filtro1, false)
 check3.addEventListener('change', filtro1, false)
 check4.addEventListener('change', filtro1, false)
 check5.addEventListener('change', filtro1, false)
 check6.addEventListener('change', filtro1, false) 
 
+
+
+*/
+let check1 = document.getElementById("coca");
+check1.addEventListener("clcik", filtro1, false);
 function filtro1() {
-  var checked = check1.checked;
+  const checked = check1.checked;
   if (checked) {
-    alert('dio click')
+    
   }
-} */
+ 
+}
+
+
+
+const prueba = document.getElementsByClassName('cocaCola').length
+ prueba.style
+console.log(prueba.style)
 
 
 
